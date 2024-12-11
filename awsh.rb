57e824ecf170055201b12fa4c0fc2ee5cc52d747
Shane da Silva <shane@dasilva.io>
2024-12-10 17:57:41 -0800

    Add sha256 checksum to silence warning during install

--- awsh.rb
+++ awsh.rb
@@ -5,6 +5,7 @@ AWSH_VERSION = '1.0.0'
 class Awsh < Formula
   homepage 'https://github.com/warpcast/awsh'
   url "https://github.com/warpcast/awsh/releases/download/v#{AWSH_VERSION}/awsh"
+  sha256 "6c77ec1c5c0195905df14d309fa1b53fc226a4432ae82bf6fbb088cbdb60d717"
 
   depends_on 'awscli'
   depends_on 'fzf'
