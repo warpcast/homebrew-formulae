7cde8d1d4fad61bb9fbc33d109ec62e7fba66ce5
Shane da Silva <shane@dasilva.io>
2024-12-11 01:07:07 -0800

    Update awsh 1.0.1 to 1.0.2

--- awsh.rb
+++ awsh.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
-AWSH_VERSION = '1.0.1'
+AWSH_VERSION = '1.0.2'
 
 class Awsh < Formula
   homepage 'https://github.com/warpcast/awsh'
   url "https://github.com/warpcast/awsh/releases/download/v#{AWSH_VERSION}/awsh"
-  sha256 "7c1256931185643c0458973e113305f725912ef271c2f402a5e28ccfed4f74bc"
+  sha256 "6ec863dd2ec0851577378546ac96bdd72c2e152b7efc8c9be69c252e065f4eb3"
 
   depends_on 'awscli'
   depends_on 'fzf'
